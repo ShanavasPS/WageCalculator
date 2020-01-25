d57d50f17b63470c51b4a9fe46de514598800136
Shanavas Shaji <shanavascruise@gmail.com>
2020-01-26 01:26:46 +0200

    added navigation

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-AppRegistry.registerComponent(appName, () => App);
+import Loading from "./src/screens/Loading"; // the loading screen
+
+import "./loadIcons"; // file for loading the icons to be used in the bottom tab navigation
+
+Navigation.registerComponent(`LoadingScreen`, () => Loading);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  // set the root component
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "LoadingScreen"
+      }
+    }
+  });
+});
