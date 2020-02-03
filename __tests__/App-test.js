865e26970744fa78b3d26d2775b7f815e7dfaba3
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-04 00:47:49 +0200

    Added few tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,37 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import "react-native";
+import React from "react";
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { getOvertimeHours, getEveningHours, calculateEveningHours } from "../src/screens/Calculator";
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it("getOvertimeHours should return 6 for input 14", () => {
+  let overtime = getOvertimeHours(14);
+  expect(overtime).toEqual(6);
+});
+
+it("getOvertimeHours should return 0 for input 8", () => {
+  let overtime = getOvertimeHours(8);
+  expect(overtime).toEqual(0);
+});
+
+it("getOvertimeHours should return 0 for input < 8", () => {
+  let overtime = getOvertimeHours(5);
+  expect(overtime).toEqual(0);
+});
+
+it("getEveningHours should return 0 for input 6", () => {
+  let hours = getEveningHours(6);
+  expect(hours).toEqual(0);
+});
+
+it("getEveningHours should return 1 for input 5", () => {
+  let hours = getEveningHours(5);
+  expect(hours).toEqual(1);
+});
+
+it("getEveningHours should return 1 for input 23", () => {
+  let hours = getEveningHours(23);
+  expect(hours).toEqual(4);
 });
