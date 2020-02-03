0d2a9d57ad8cf143d01e962babaf0e8a2ce7ddeb
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-03 22:36:00 +0200

    Renamed project name to MonthlyWage

--- ios/MonthlyWageTests/MonthlyWageTests.m
+++ ios/MonthlyWageTests/MonthlyWageTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface navigation_react_nativeTests : XCTestCase
+@interface MonthlyWageTests : XCTestCase
 
 @end
 
-@implementation navigation_react_nativeTests
+@implementation MonthlyWageTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
