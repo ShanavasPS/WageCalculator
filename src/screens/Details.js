114f3baadf931eb4fa02d321ae98304959191359
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-04 15:31:08 +0200

    Updated listview styling

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -7,16 +7,10 @@
  */
 
 import React, { Component } from "react";
-import {
-  FlatList,
-  View,
-  Text,
-  StyleSheet
-} from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
 import { ListItem } from "react-native-elements";
 
 export default class Details extends Component {
-
   static get options() {
     return {
       topBar: {
@@ -26,17 +20,18 @@ export default class Details extends Component {
           fontSize: 18
         },
         background: {
-          color: '#FBFCFC'
-        },
+          color: "#FBFCFC"
+        }
       }
     };
   }
 
-  renderHeader = (name) => (
+  renderHeader = name =>
     <View style={styles.header}>
-      <Text style={styles.headerText}>Wage Details for {name}</Text>
-    </View>
-  )
+      <Text style={styles.headerText}>
+        Wage Details for {name}
+      </Text>
+    </View>;
 
   render() {
     const { name, detailedShifts } = this.props;
@@ -44,19 +39,38 @@ export default class Details extends Component {
       <View style={styles.wrapper}>
         <FlatList
           data={detailedShifts}
-          renderItem={({ item, index }) => (
-
+          renderItem={({ item, index }) =>
             <ListItem
               key={index}
               title={item.date}
-              rightTitle={'$' + item.wage}
-              subtitle={'Total\nEvening\nOvertime'}
-              subtitleStyle={styles.subtitle}
-              rightSubtitle={item.totalHours + 'h\n' + item.eveningHours + 'h\n' + item.overtimeHours + 'h'}
+              rightTitle={
+                  <Text style={styles.rightTitle}>
+                    ${item.wage}
+                  </Text>
+              }
+              subtitle={
+                <View style={styles.subtitle}>
+                  <Text>Total Hours</Text>
+                  <Text>Evening Hours</Text>
+                  <Text>Overtime Hours</Text>
+                </View>
+              }
+              rightSubtitle={
+                <View style={styles.rightSubtitle}>
+                  <Text style={styles.rightSubtitle}>
+                    {item.totalHours} h
+                  </Text> 
+                  <Text style={styles.rightSubtitle}>
+                    {item.eveningHours} h
+                  </Text>
+                  <Text style={styles.rightSubtitle}>
+                    {item.overtimeHours} h
+                  </Text>
+                </View>
+              }
+              containerStyle={{ alignItems: "flex-end" }}
               bottomDivider
-            />
-
-          )}
+            />}
           keyExtractor={item => item.date}
           ListHeaderComponent={this.renderHeader(name)}
           stickyHeaderIndices={[0]}
@@ -77,14 +91,23 @@ const styles = StyleSheet.create({
     fontSize: 14,
     padding: 5
   },
+  rightSubtitle: {
+    alignItems: "flex-end",
+    color: "#808080"
+  },
+  rightTitle: {
+    alignItems: "flex-end",
+    fontSize: 16,
+    paddingVertical: 5,
+  },
   header: {
     padding: 20,
-    borderBottomColor: '#D0D3D4',
+    borderBottomColor: "#D0D3D4",
     borderBottomWidth: 0.5,
-    backgroundColor: '#FFFFFF',
-    borderTopColor: '#D0D3D4',
+    backgroundColor: "#FFFFFF",
+    borderTopColor: "#D0D3D4",
     borderTopWidth: 0.5,
-    alignItems: "center",
+    alignItems: "center"
   },
   headerText: {
     fontSize: 16,
